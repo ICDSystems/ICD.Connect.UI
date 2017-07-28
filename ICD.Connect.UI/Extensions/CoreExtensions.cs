5c5be91fb01548af3349f4eb5e13f0c3a8f2403b
Chris Cameron <chris.cameron@wedoresi.com>
2017-07-28 12:25:24 -0400

    Optimization in IUserInterfaceFactory access

--- ICD.Connect.UI/Extensions/CoreExtensions.cs
+++ ICD.Connect.UI/Extensions/CoreExtensions.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using ICD.Connect.Settings;
 using ICD.Connect.Settings.Core;
 
@@ -15,11 +14,12 @@ namespace ICD.Connect.UI.Extensions
 		{
 		}
 	}
+
 	public static class CoreExtensions
 	{
 		public static UserInterfaceFactoryCollection GetUiFactories(this ICore core)
 		{
-			return new UserInterfaceFactoryCollection(core.Originators.OfType<IUserInterfaceFactory>());
+			return new UserInterfaceFactoryCollection(core.Originators.GetChildren<IUserInterfaceFactory>());
 		}
 	}
 }
