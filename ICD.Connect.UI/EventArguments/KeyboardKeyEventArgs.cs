e495b3016063c6b18a0fa8322ce07280689b486c
Chris Cameron <chris.cameron@wedoresi.com>
2018-02-27 10:28:43 -0500

    Initial commit of KeyboardKey and event args

---
+++ ICD.Connect.UI/EventArguments/KeyboardKeyEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using ICD.Common.Utils.EventArguments;
+using ICD.Connect.UI.Utils;
+
+namespace ICD.Connect.UI.EventArguments
+{
+	public sealed class KeyboardKeyEventArgs : GenericEventArgs<KeyboardKey>
+	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="data"></param>
+		public KeyboardKeyEventArgs(KeyboardKey data)
+			: base(data)
+		{
+		}
+	}
+}
---
+++ ICD.Connect.UI/Utils/KeyboardKey.cs
@@ -0,0 +1,48 @@
+﻿namespace ICD.Connect.UI.Utils
+{
+	/// <summary>
+	/// Simple pairing of uppercase and lowercase chars.
+	/// </summary>
+	public struct KeyboardKey
+	{
+		private readonly char m_Lower;
+		private readonly char m_Upper;
+
+		/// <summary>
+		/// Returns true if the character is a letter.
+		/// </summary>
+		private bool IsLetter { get { return char.IsLetter(m_Lower); } }
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="lower"></param>
+		public KeyboardKey(char lower)
+			: this(lower, char.ToUpper(lower))
+		{
+		}
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="lower"></param>
+		/// <param name="upper"></param>
+		public KeyboardKey(char lower, char upper)
+		{
+			m_Lower = lower;
+			m_Upper = upper;
+		}
+
+		/// <summary>
+		/// Returns the character based on the shift/caps state.
+		/// </summary>
+		/// <param name="shift"></param>
+		/// <param name="caps"></param>
+		/// <returns></returns>
+		public char GetChar(bool shift, bool caps)
+		{
+			bool upper = IsLetter ? shift ^ caps : shift;
+			return upper ? m_Upper : m_Lower;
+		}
+	}
+}
