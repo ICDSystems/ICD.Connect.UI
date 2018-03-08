e14057a17d620a8148311984d4bd126b1a2a0048
Chris Cameron <chris.cameron@wedoresi.com>
2018-03-08 10:49:34 -0500

    Tidying

--- ICD.Connect.UI/Properties/AssemblyInfo.cs
+++ ICD.Connect.UI/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 [assembly: AssemblyTitle("ICD.Connect.UI")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.UI")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2017")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
 [assembly: AssemblyVersion("2.0.0.0")]
