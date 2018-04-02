d63c0035e442fdf4934ad0df3b738e565e1d2350
Jack Kanarish <jack.kanarish@wedoresi.com>
2018-04-02 15:00:41 -0400

    new versioning commit

--- ICD.Connect.UI/Properties/AssemblyInfo.cs
+++ ICD.Connect.UI/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+using System.Reflection;
 
 [assembly: AssemblyTitle("ICD.Connect.UI")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.UI")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
