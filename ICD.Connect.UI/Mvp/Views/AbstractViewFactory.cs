ba8626dc27b21d10a4df7476bc70c84246452896
Chris Cameron <chris.cameron@wedoresi.com>
2018-10-18 16:48:38 -0400

    refactor: Tidying

--- ICD.Connect.UI/Mvp/Views/AbstractViewFactory.cs
+++ ICD.Connect.UI/Mvp/Views/AbstractViewFactory.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using ICD.Connect.Panels;
 using ICD.Connect.Panels.Devices;
 using ICD.Connect.Panels.SmartObjects;
 using ICD.Connect.UI.Controls;
