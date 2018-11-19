e544da31affdacc3ab1cd5c341a8ee8bd69eafd0
Chris Cameron <chris.cameron@wedoresi.com>
2018-11-19 13:38:36 -0500

    refactor: Removing unused code

--- ICD.Connect.UI/Mvp/Views/IViewFactory.cs
+++ ICD.Connect.UI/Mvp/Views/IViewFactory.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using ICD.Connect.Panels.Devices;
-using ICD.Connect.Panels.SmartObjects;
-using ICD.Connect.UI.Controls;
 using ICD.Connect.UI.Controls.Lists;
 
 namespace ICD.Connect.UI.Mvp.Views
@@ -22,16 +20,6 @@ namespace ICD.Connect.UI.Mvp.Views
 		/// <typeparam name="T"></typeparam>
 		/// <returns></returns>
 		T GetNewView<T>() where T : class, IView;
-
-		/// <summary>
-		/// Instantiates a view of the given type.
-		/// </summary>
-		/// <typeparam name="T"></typeparam>
-		/// <param name="smartObject"></param>
-		/// <param name="parent"></param>
-		/// <param name="index"></param>
-		/// <returns></returns>
-		T GetNewView<T>(ISmartObject smartObject, IVtProParent parent, ushort index) where T : class, IView;
 
 		/// <summary>
 		/// Creates views for the given subpage reference list.
