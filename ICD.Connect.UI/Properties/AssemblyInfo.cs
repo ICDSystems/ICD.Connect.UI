9736b81e68725d0e5d5d5c0da3c82f216bfd20fc
Chris Cameron <chris.cameron@wedoresi.com>
2019-04-05 15:54:41 -0400

    chore: updating changelog, incrementing minor version

--- ICD.Connect.UI/Properties/AssemblyInfo.cs
+++ ICD.Connect.UI/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.UI")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.UI")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2018")]
-[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2019")]
+[assembly: AssemblyVersion("5.1.0.0")]
