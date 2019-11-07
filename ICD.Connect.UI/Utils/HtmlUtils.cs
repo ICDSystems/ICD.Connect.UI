037af53056579a7a1f47f1c86f3ca4e3b54f95fe
Chris Cameron <chris.cameron@wedoresi.com>
2019-11-07 17:59:15 -0500

    feat: Dirty method for adding a font size element to a piece of text

--- ICD.Connect.UI/Utils/HtmlUtils.cs
+++ ICD.Connect.UI/Utils/HtmlUtils.cs
@@ -21,10 +21,21 @@ namespace ICD.Connect.UI.Utils
 		/// <returns></returns>
 		public static string FormatColoredText(string text, string color)
 		{
-			if (string.IsNullOrEmpty("color"))
+			if (string.IsNullOrEmpty(color))
 				throw new ArgumentException("Color is null or empty", "color");
 
 			return string.IsNullOrEmpty(text) ? text : string.Format("<font color=\"{0}\">{1}</font>", color, text);
+		}
+
+		/// <summary>
+		/// Wraps the text with a font element with a size attribute.
+		/// </summary>
+		/// <param name="text"></param>
+		/// <param name="size"></param>
+		/// <returns></returns>
+		public static string FormatFontSize(string text, int size)
+		{
+			return string.IsNullOrEmpty(text) ? text : string.Format("<font size=\"{0}\">{1}</font>", size, text);
 		}
 
 		/// <summary>
@@ -34,7 +45,13 @@ namespace ICD.Connect.UI.Utils
 		/// <returns></returns>
 		public static string ReplaceNewlines(string text)
 		{
-			return text == null ? null : text.Replace(IcdEnvironment.NewLine, LINE_BREAK);
+			if (text == null)
+				return text;
+
+			text = text.Replace(IcdEnvironment.NewLine, LINE_BREAK);
+			text = text.Replace("\n", LINE_BREAK);
+
+			return text;
 		}
 	}
 }
