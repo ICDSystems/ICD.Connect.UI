3960774a61fc3d1023d8c17d6bb05c6103dcae3c
Drew Tingen <drew.tingen@icdsystems.com>
2021-08-18 11:38:28 -0400

    chore: Update Changelog, increment major version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [7.0.0] - 2021-08-18
+### Changed
+ - Updated dependencies
+
 ## [6.7.0] - 2021-01-14
 ### Added
  - Added VtProInternetExplorer control
--- ICD.Connect.UI/Properties/AssemblyInfo.cs
+++ ICD.Connect.UI/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.UI")]
 [assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
-[assembly: AssemblyVersion("6.7.0.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
