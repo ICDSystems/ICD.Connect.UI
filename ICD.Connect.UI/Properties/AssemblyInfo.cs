5e5974b7a546fef3d85daeccc5f9c9892949589c
Chris Cameron <chris.cameron@icdsystems.com>
2022-01-05 16:27:18 -0500

    chore: Updating AssemblyInfo year

--- ICD.Connect.UI/Properties/AssemblyInfo.cs
+++ ICD.Connect.UI/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyTitle("ICD.Connect.UI")]
 [assembly: AssemblyCompany("ICD Systems")]
 [assembly: AssemblyProduct("ICD.Connect.UI")]
-[assembly: AssemblyCopyright("Copyright © ICD Systems 2021")]
+[assembly: AssemblyCopyright("Copyright © ICD Systems 2022")]
 [assembly: AssemblyVersion("7.0.0.0")]
